3aa6ae03f87a69f366d01c0b34c9feeed5a9c94b
BlackYakup <yakup.acar@outlook.de>
2026-04-28 12:50:28 +0200

    Getter and Setter added

--- triangle.dart
+++ triangle.dart
@@ -1,73 +1,55 @@
 class Triangle {
-  double heightInMm;
-  double widthInMm;
+  double _heightInMm;
+  double _widthInMm;
   MeasurementSystem measurementSystem;
 
-  Triangle(
-    this.heightInMm,
-    this.widthInMm,) 
-    : measurementSystem = MeasurementSystem.mm;
+  // Standard-Konstruktor
+  Triangle(this._heightInMm, this._widthInMm) : measurementSystem = MeasurementSystem.mm;
 
-  Triangle.cm(double height, double width)
-    : heightInMm = height * 10,
-      widthInMm = width * 10,
-      measurementSystem = MeasurementSystem.cm;
+  // Benannte Konstruktoren nutzen jetzt die Faktoren aus dem Enum
+  Triangle.feet(double h, double w) : this.fromSystem(h, w, MeasurementSystem.feet);
+  Triangle.inch(double h, double w) : this.fromSystem(h, w, MeasurementSystem.inch);
+  Triangle.m(double h, double w) : this.fromSystem(h, w, MeasurementSystem.m);
+  Triangle.dm(double h, double w) : this.fromSystem(h, w, MeasurementSystem.dm);
+  Triangle.cm(double h, double w) : this.fromSystem(h, w, MeasurementSystem.cm);
+  Triangle.mm(double h, double w) : this.fromSystem(h, w, MeasurementSystem.mm);
+  // ... usw für inch/feet
 
-  Triangle.dm(double height, double width)
-    : heightInMm = height * 100,
-      widthInMm = width * 100,
-      measurementSystem = MeasurementSystem.dm;
+  // Ein interner Hilfskonstruktor spart Schreibarbeit
+  Triangle.fromSystem(double h, double w, this.measurementSystem)
+      : _heightInMm = h * measurementSystem.factorToMm,
+        _widthInMm = w * measurementSystem.factorToMm;
 
-  Triangle.m(double height, double width)
-    : heightInMm = height * 1000,
-      widthInMm = width * 1000,
-      measurementSystem = MeasurementSystem.m;
+  double get heightInMeters => _heightInMm / MeasurementSystem.m.factorToMm;
+  double get heightInFeet => _heightInMm / MeasurementSystem.feet.factorToMm;
+  
+  double get height => _heightInMm / measurementSystem.factorToMm;
+  double get width => _widthInMm / measurementSystem.factorToMm;
 
-  Triangle.inch(double height, double width)
-    : heightInMm = height * 25.4,
-      widthInMm = width * 25.4,
-      measurementSystem = MeasurementSystem.inch;
-
-  Triangle.feet(double height, double width)
-    : heightInMm = height * 304.8,
-      widthInMm = width * 304.8,
-      measurementSystem = MeasurementSystem.feet;
+  set heightInMeters(double value) => _heightInMm = value * MeasurementSystem.m.factorToMm;
+  set widthInInches(double value) => _widthInMm = value * MeasurementSystem.inch.factorToMm;
 
   @override
   String toString() {
-    return 'Dreieck ${measurementSystem.ausgeschriebeneEinheiten()}: H=${heightInMm}$measurementSystem, B=${widthInMm}$measurementSystem';
+    return 'Dreieck ${measurementSystem.description}: H=${height.toStringAsFixed(2)}$measurementSystem, B=${width.toStringAsFixed(2)}$measurementSystem';
   }
 }
 
 enum MeasurementSystem {
-  mm,
-  cm,
-  dm, 
-  m,
-  inch,
-  feet;
+  mm(1.0, "mm", "in Millimeter"),
+  cm(10.0, "cm", "in Zentimeter"),
+  dm(100.0, "dm", "in Dezimeter"),
+  m(1000.0, "m", "in Meter"),
+  inch(25.4, "inch", "in Zoll"),
+  feet(304.8, "feet", "in Fuß");
+
+  // Das ist der "Umrechnungsfaktor" zur Basis (Millimeter)
+  final double factorToMm;
+  final String symbol;
+  final String description;
+
+  const MeasurementSystem(this.factorToMm, this.symbol, this.description);
 
   @override
-  String toString() {
-    switch(this){
-      case MeasurementSystem.mm: return "mm";
-      case MeasurementSystem.cm: return "cm";
-      case MeasurementSystem.dm: return "dm";
-      case MeasurementSystem.m: return "m";
-      case MeasurementSystem.inch: return "inch";
-      case MeasurementSystem.feet: return "feet";
-    }
-  }
-
-  String ausgeschriebeneEinheiten() {
-    switch(this){
-      case MeasurementSystem.mm: return "in Millimeter";
-      case MeasurementSystem.cm: return "in Zentimeter";
-      case MeasurementSystem.dm: return "in Dezimeter";
-      case MeasurementSystem.m: return "in Meter";
-      case MeasurementSystem.inch: return "in Zoll";
-      case MeasurementSystem.feet: return "in Fuß";
-    }
-  }
+  String toString() => symbol;
 }
-
